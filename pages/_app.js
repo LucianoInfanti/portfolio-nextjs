26b8bd78edca7d7644fc6c1bf4fbadcb2237197f
LucianoInfanti <lucianoinfanti369@gmail.com>
2023-05-09 13:19:30 -0300

    removing the AnimatePresence from app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,11 @@
 import "../styles/global.css";
-import { motion, AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps, router }) {
   return (
     <>
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={router.pathname}
-          initial="initialState"
-          animate="animateState"
-          exit="exitState"
-          variants={{
-            initialState: {
-              opacity: 0,
-            },
-            animateState: {
-              opacity: 1,
-            },
-            exitState: {
-              opacity: 0,
-            },
-          }}
-          transition={{
-            duration: 0.5,
-            ease: "easeInOut",
-          }}
-        >
+        
           <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
+        
     </>
   );
 }
